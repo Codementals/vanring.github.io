cefcf6a7eb7416ffe79743660d22d49baaba5c29
the-online-mercedes <96192633+the-online-mercedes@users.noreply.github.com>
2024-12-11 14:27:09 -0600

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,12 +1,8 @@
-<!DOCTYPE HTML>
-<html>  
-    <body>
-    
-        <form action="welcome.php" method="post">
-            Name: <input type="text" name="name"><br>
-            E-mail: <input type="text" name="email"><br>
-            <input type="submit">
-        </form>
-    
-    </body>
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
 </html>
